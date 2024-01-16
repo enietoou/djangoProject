c69eeb86b81721badde827dac988b53e1d0b7ec1
Aziz <enietogou@gmail.com>
2024-01-16 12:22:43 +0500

    Добавлена страница Востребованность и География

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -1,9 +1,11 @@
 
 from django.contrib import admin
 from django.urls import path
-from mainapp.views import home
+from mainapp.views import home, demand
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
+    path('demand/', demand, name='demand'),
+    path('geography/', demand, name='geography'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -4,3 +4,9 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def demand(request):
+    return render(request, 'demand.html')
+
+def geography(request):
+    return render(request, 'geography.html')
