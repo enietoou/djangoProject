8479db620db2853e7477709c7ee1a28a61b993f1
Aziz <enietogou@gmail.com>
2024-01-19 16:27:59 +0500

    Добавлены хосты

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -114,10 +114,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-# # for local
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATIC_ROOT = os.path.join('static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "mainapp/static"),
